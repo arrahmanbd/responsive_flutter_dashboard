126a12b8e6f3d759caa2717dc5e0af8238215528
AR Rahman <74553735+arrahmanbd@users.noreply.github.com>
2023-05-08 17:40:31 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dashboard/core/constants/colors.dart';
 import 'package:flutter_dashboard/dashboard.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -22,25 +21,6 @@ class MyApp extends ConsumerWidget {
       themeMode: toggle?ThemeMode.dark:ThemeMode.light,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      // ThemeData(
-      //     primaryColor: MaterialColor(
-      //       primaryColorCode,
-      //       <int, Color>{
-      //         50: const Color(primaryColorCode).withOpacity(0.1),
-      //         100: const Color(primaryColorCode).withOpacity(0.2),
-      //         200: const Color(primaryColorCode).withOpacity(0.3),
-      //         300: const Color(primaryColorCode).withOpacity(0.4),
-      //         400: const Color(primaryColorCode).withOpacity(0.5),
-      //         500: const Color(primaryColorCode).withOpacity(0.6),
-      //         600: const Color(primaryColorCode).withOpacity(0.7),
-      //         700: const Color(primaryColorCode).withOpacity(0.8),
-      //         800: const Color(primaryColorCode).withOpacity(0.9),
-      //         900: const Color(primaryColorCode).withOpacity(1.0),
-      //       },
-      // ),
-      // scaffoldBackgroundColor: Color(0xFF171821),
-      // fontFamily: 'IBMPlexSans',
-      // brightness: Brightness.dark),
       home: DashBoard(),
     );
   }
